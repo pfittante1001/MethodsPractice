970b75d10246085f54d597e5d0efd2f842ac7a33
pfittante <pfittante@yahoo.com>
2017-07-10 21:30:10 -0400

    Methods Practice cont

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -9,7 +9,7 @@ namespace MethodsPractice
     class Program
     {
 
-       
+
         static void Main(string[] args)
         {
             //Hello();
@@ -20,47 +20,73 @@ namespace MethodsPractice
             //int age = 1;
             //int retireYears = RetirementCalc(age);
 
-            int value = ParseCheck("test");
-            Console.WriteLine("This is a valid number {0}",value);
-            //Console.WriteLine("The user will retire in {0} years where {1} represents the value calculate.", retireYears, age);
+
+            //int value = ParseCheck("test");
+            //Console.WriteLine("This is a valid number {0}",value);
+
+            //Console.WriteLine("Please enter two numbers to add: ");
+            //int[] sumArray = new int[2];
+
+            //for (int i = 0; i <=1; i++)
+            //{
+            //    Console.Write("Please enter your number: ");
+            //    sumArray[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine(sumIt(sumArray[0], sumArray[1]));
+
+            //Console.WriteLine("Home many number would you like to sum? ");
+            //int numAdd = int.Parse(Console.ReadLine());
+            //int[] sumArray = new int[numAdd];
+
+            //Console.WriteLine("Please enter {0} numbers to add: ", numAdd);
+            //for (int i = 0; i <sumArray.Length; i++)
+            //{
+            //    Console.Write("Please enter your numbers: ");
+            //    sumArray[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int total = sumIt(sumArray, numAdd);
+            //Console.WriteLine(total);   
+
+            Console.WriteLine(doubleIt(7));       
         }
 
-        public static int ParseCheck(string randomStr)
-        {
-           
-            int tempInt;
-            while (int.TryParse(randomStr,out tempInt)== false)
-            {
-                Console.WriteLine("Please enter a valid number");
-                randomStr = Console.ReadLine();
-            }
-            return tempInt;
-        }
-        public static void FavoriteFood( string name, string food)
-        {
-            Console.WriteLine("{0} favorite food is {1} ", name, food);
-        }
+        //public static int ParseCheck(string randomStr)
+        //{
 
-        public static int RetirementCalc(int userAge)
-        {
-            int years;
-            switch (userAge)
-            {
-                case 1:
-                    years = 2;
-                    break;
-                case 2:
-                    years = 2;
-                    break;
-                default:
-                    years = 50;
-                    break;            
-                    
-            }
+        //    int tempInt;
+        //    while (int.TryParse(randomStr,out tempInt)== false)
+        //    {
+        //        Console.WriteLine("Please enter a valid number");
+        //        randomStr = Console.ReadLine();
+        //    }
+        //    return tempInt;
+        //}
+        //public static void FavoriteFood( string name, string food)
+        //{
+        //    Console.WriteLine("{0} favorite food is {1} ", name, food);
+        //}
 
-            return years;
-        }
-       //Example of a void methods which doesnt return an into or double ets
+        //public static int RetirementCalc(int userAge)
+        //{
+        //    int years;
+        //    switch (userAge)
+        //    {
+        //        case 1:
+        //            years = 2;
+        //            break;
+        //        case 2:
+        //            years = 2;
+        //            break;
+        //        default:
+        //            years = 50;
+        //            break;            
+
+        //    }
+
+        //    return years;
+        //}
+        //Example of a void methods which doesnt return an into or double ets
         //static void Hello()
         //{
         //    Console.Write("Hello ");
@@ -84,10 +110,31 @@ namespace MethodsPractice
         //    return sum;
         //}
 
-       
+        //public static int sumIt(int num1, int num2)
+        //{
+        //    int total = num1 + num2;
+
+        //    return total;
+        //}
+         
+        //public static int sumIt(int[] newArray, int size)
+        //{
+        //    int sum = 0;
+        //   for (int i = 0; i < size; i++)
+        //    {
+        //       sum += newArray[i];
+        //    }
+
+        //    return sum;
+        //}
+
+        public static int doubleIt(int num1)
+        {
+            int total = 0;
+            total = num1 * 2;
+            return total;
+        }
+
+
     }
-
-
-
-
 }
